d0dd98becf9c7767d3e4706eb142be917ef140dd
AugustineChulu <chuluaugustine@gmail.com>
2026-03-01 09:31:52 +0200

    url restore

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig(() => {
-  const projectName = 'test_monita_et';
+  const projectName = 'monita';
   const outDir = process.env.GITHUB_ACTIONS ? 'dist' : `dist/${projectName}`;
 
   return {
